5beb4405d8d8c6da47d7117a1d2835d7d13dfb8a
Srikanth <srikanthh994@gmail.com>
2021-09-30 13:09:29 +0530

    Added:LeapYear Or Not

--- LeapYear.java
+++ LeapYear.java
@@ -1,32 +1,17 @@
-	package corebasics;
-	import java.util.Scanner;
+package corebasics;
+import java.util.Scanner;
 
-	public class LeapYear {
-		public static void main(String[] args) {
-
-			//entering the years
-			System.out.println("Enter the year: ");
-			Scanner sc = new Scanner(System.in);
-			int year = sc.nextInt();
-			if (checkYear(year))
-				checkLeapyear(year);
-			else
-				System.out.println("Enter the valid four digit number");
-			sc.close();
-		}
-
-		public static boolean checkYear(int year) {
-			if (year > 999)
-				return false;
-			return true;
-		}
-
-		//logic for Leap Year calculation
-		public static void checkLeapyear(int year) {
-			if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
-				System.out.println("The year is Leap Year");
-			} else
-				System.out.println("The Year is Not a Leap Year");
-
-		}
+public class LeapYear {
+	public static void main(String[] args){
+		int year;
+		System.out.println("Enter an Year :: ");
+		Scanner sc = new Scanner(System.in);
+		year = sc.nextInt();
+		sc.close();
+		if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+			System.out.println("Specified year is a leap year");
+		else
+			System.out.println("Specified year is not a leap year");
 	}
+}
+
